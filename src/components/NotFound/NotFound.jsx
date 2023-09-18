3484feb5980c56adfbc1362e413e44dbc2b59a56
AlekseyDream <u550@list.ru>
2023-09-18 18:40:46 +0300

    edit 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -10,7 +10,7 @@ function NotFound({}) {
       <h1 className="error__title">404</h1>
       <p className="error__text">Страница не найдена</p>
       <button
-      onClick={() => navigate(-3)}
+      onClick={() => navigate(-1)}
       className="error__button"
       >
         Назад
